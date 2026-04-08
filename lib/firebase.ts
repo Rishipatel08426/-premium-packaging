c36dcce183fdc43dc5412f4fecf1ff4f8f0f8452
Rishipatel08426 <rishipatel2580@gmail.com>
2026-04-08 21:41:08 +0530

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,12 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBFQ_xxvIW1dcRKzhaWULdNb7gEEpe_9TU",
+  authDomain: "areca-studio.firebaseapp.com",
+  databaseURL: "https://areca-studio-default-rtdb.firebaseio.com",
+  projectId: "areca-studio",
+  storageBucket: "areca-studio.firebasestorage.app",
+  messagingSenderId: "801485446463",
+  appId: "1:801485446463:web:915b8a6a7c5011d500478a",
+  measurementId: "G-M2Q10TERDP"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
